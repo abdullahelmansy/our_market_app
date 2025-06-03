06f1434fe50e0b4c69ae3ab610e0ecf03b2f5df9
abdullahelmansy <abdullahelmansy03@gmail.com>
2025-06-03 20:41:59 +0300

    setup app and add colors for app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:our_market_app/feature/auth/ui/login_view.dart';
+
+void main() {
+  runApp(const OurMarket());
+}
+
+class OurMarket extends StatelessWidget {
+  const OurMarket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Our Market App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
