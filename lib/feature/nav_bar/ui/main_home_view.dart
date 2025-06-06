7990c746fc0bf89e712b815c041e75db63fe650c
abdullahelmansy <abdullahelmansy03@gmail.com>
2025-06-06 21:00:27 +0300

    use cuibt state in nav bar

--- lib/feature/nav_bar/ui/main_home_view.dart
+++ lib/feature/nav_bar/ui/main_home_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:our_market_app/core/utils/app_color.dart';
 import 'package:our_market_app/feature/favorite/ui/favorite_view.dart';
 import 'package:our_market_app/feature/home/ui/home_view.dart';
+import 'package:our_market_app/feature/nav_bar/logic/cubit/nav_bar_cubit.dart';
 import 'package:our_market_app/feature/profile/ui/profile_view.dart';
 import 'package:our_market_app/feature/store/ui/store_voew.dart';
 
@@ -16,34 +18,49 @@ class MainHomeView extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: screens[0]),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-        child: GNav(
-          rippleColor:
-              AppColors.kPrimaryColor, // tab button ripple color when pressed
-          hoverColor: AppColors.kPrimaryColor, // tab button hover color
-
-          curve: Curves.easeOutExpo, // tab animation curves
-          duration: Duration(milliseconds: 400), // tab animation duration
-          gap: 8, // the tab button gap between icon and text
-          color: AppColors.kGreyColor, // unselected icon color
-          activeColor: AppColors.kWhiteColor, // selected icon and text color
-          iconSize: 24, // tab button icon size
-          tabBackgroundColor:
-              AppColors.kPrimaryColor, // selected tab background color
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 10,
-          ), // navigation bar padding
-          tabs: [
-            GButton(icon: Icons.home, text: 'Home'),
-            GButton(icon: Icons.store, text: 'Store'),
-            GButton(icon: Icons.favorite, text: 'Favorites'),
-            GButton(icon: Icons.person, text: 'Profile'),
-          ],
-        ),
+    return BlocProvider(
+      create: (context) => NavBarCubit(),
+      child: BlocBuilder<NavBarCubit, NavBarState>(
+        builder: (context, state) {
+          var cubit = context.read<NavBarCubit>();
+          return Scaffold(
+            body: SafeArea(
+              child:
+                  cubit.currentIndex == 0
+                      ? screens[0]
+                      : screens[cubit.currentIndex],
+            ),
+            bottomNavigationBar: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+              child: GNav(
+                onTabChange: (value) => cubit.changeIndex(value),
+                rippleColor:
+                    AppColors
+                        .kPrimaryColor, // tab button ripple color when pressed
+                hoverColor: AppColors.kPrimaryColor, // tab button hover color
+                curve: Curves.easeOutExpo, // tab animation curves
+                duration: Duration(milliseconds: 400), // tab animation duration
+                gap: 8, // the tab button gap between icon and text
+                color: AppColors.kGreyColor, // unselected icon color
+                activeColor:
+                    AppColors.kWhiteColor, // selected icon and text color
+                iconSize: 24, // tab button icon size
+                tabBackgroundColor:
+                    AppColors.kPrimaryColor, // selected tab background color
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ), // navigation bar padding
+                tabs: [
+                  GButton(icon: Icons.home, text: 'Home'),
+                  GButton(icon: Icons.store, text: 'Store'),
+                  GButton(icon: Icons.favorite, text: 'Favorites'),
+                  GButton(icon: Icons.person, text: 'Profile'),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
