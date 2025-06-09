e9d543e45a80394a03ef767df95d0590dd7c6e63
abdullahelmansy <abdullahelmansy03@gmail.com>
2025-06-09 19:00:49 +0300

    finish favorite view ui

--- lib/feature/favorite/ui/favorite_view.dart
+++ lib/feature/favorite/ui/favorite_view.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:our_market_app/core/widgets/product_card_widget.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('favorites'));
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              'Your Favorite Products',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 15),
+          ProductCard(),
+        ],
+      ),
+    );
   }
 }
