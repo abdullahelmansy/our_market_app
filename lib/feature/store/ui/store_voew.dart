454610b645f4aea56e79659b42b60f645dbd6325
abdullahelmansy <abdullahelmansy03@gmail.com>
2025-06-09 18:47:14 +0300

    finish store view ui

--- lib/feature/store/ui/store_voew.dart
+++ lib/feature/store/ui/store_voew.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:our_market_app/core/widgets/custom_search_feild.dart';
+import 'package:our_market_app/core/widgets/product_list_widgets.dart';
 
 class StoreView extends StatelessWidget {
   const StoreView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Store'));
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              'Welcome to Our Market',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 15),
+          CustomSearchFeild(),
+          SizedBox(height: 15),
+          ProductsListWidget(),
+        ],
+      ),
+    );
   }
 }
